67b433ee04e90ca40778a5749918811de1e109b5
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-01 10:09:18 +0800

    onGenerateRoute & onUnknownRoute

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,14 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        print(settings.name);
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
